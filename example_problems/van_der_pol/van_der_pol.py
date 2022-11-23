added test for runnin cost gradient shapes; need to check correct by finite differences; need to add hessian

example_problems/van_der_pol/van_der_pol.py
@@ -81,7 +81,7 @@ class VanDerPol(OptimalControlProblem):
 
     def running_cost(self, x, u):
         '''
-        Evaluate the running cost L(x,u) at one or multiple state-control pairs.
+        Evaluate the running cost L(x,u) at one or more state-control pairs.
 
         Parameters
         ----------
@@ -92,16 +92,16 @@ class VanDerPol(OptimalControlProblem):
 
         Returns
         -------
-        L : (1,) or (n_points,) array
+        L : float or (n_points,) array
             Running cost(s) L(x,u) evaluated at pair(s) (x,u).
         '''
-        if x.ndim < 1:
+        if x.ndim < 2:
             x_err = x - self.xf.flatten()
         else:
             x_err = x - self.xf
 
         u = self._saturate(u)
-        if u.ndim < 1:
+        if u.ndim < 2:
             u_err = u - self.uf.flatten()
         else:
             u_err = u - self.uf
@@ -112,7 +112,7 @@ class VanDerPol(OptimalControlProblem):
         L += (self._params.Wy/2.) * x_err[1:]
         L += (self._params.Wu/2.) * u_err**2
 
-        return np.squeeze(L)
+        return L[0]
 
     def running_cost_gradients(self, x, u, return_dLdx=True, return_dLdu=True):
         '''
@@ -138,13 +138,13 @@ class VanDerPol(OptimalControlProblem):
         dLdu : (n_states,) or (n_states, n_points) array
             Control gradients dL/du (x,u) evaluated at pair(s) (x,u).
         '''
-        if x.ndim < 1:
+        if x.ndim < 2:
             x_err = x - self.xf.flatten()
         else:
             x_err = x - self.xf
 
         u = self._saturate(u)
-        if u.ndim < 1:
+        if u.ndim < 2:
             u_err = u - self.uf.flatten()
         else:
             u_err = u - self.uf
@@ -152,7 +152,7 @@ class VanDerPol(OptimalControlProblem):
         x1 = x_err[:1]
         x2 = x_err[1:]
 
-        if return_dLdX:
+        if return_dLdx:
             dLdx = np.concatenate((self._params.Wx * x1, self._params.Wy * x2))
             if not return_dLdu:
                 return dLdx
@@ -162,7 +162,7 @@ class VanDerPol(OptimalControlProblem):
             if not return_dLdx:
                 return dLdu
 
-        return dLdu, dLdu
+        return dLdx, dLdu
 
     def dynamics(self, x, u):
         '''

unit_tests/test_OptimalControlProblem.py
@@ -7,13 +7,15 @@ from optimalcontrol.problem import ProblemParameters
 
 ocp_dict = {}
 
-from examples.van_der_pol import van_der_pol
+from example_problems.van_der_pol import van_der_pol
 ocp_dict['van_der_pol'] = {
     'ocp': van_der_pol.VanDerPol, 'config': van_der_pol.config
 }
 
+rng = np.random.default_rng()
+
 @pytest.mark.parametrize('ocp_name', ocp_dict.keys())
-def test_initialize(ocp_name):
+def test_init(ocp_name):
     problem = ocp_dict[ocp_name]['ocp'](dummy_variable=False)
 
     # Check that basic properties have been implemented
@@ -22,23 +24,75 @@ def test_initialize(ocp_name):
     assert np.isinf(problem.final_time) or problem.final_time > 0.
     assert isinstance(problem.parameters, ProblemParameters)
 
-    # Check that problem parameters are updateable
+    # Check that problem parameters can be updated
     assert not problem.parameters.dummy_variable
     problem.parameters.update(dummy_variable=True)
     assert problem.parameters.dummy_variable
 
-    # Check that sample_initial_conditions returns the correct size arrays
+@pytest.mark.parametrize('ocp_name', ocp_dict.keys())
+def test_sample_initial_conditions(ocp_name):
+    problem = ocp_dict[ocp_name]['ocp']()
+
     with pytest.raises(Exception):
         problem.sample_initial_conditions(n_samples=0)
-    x0 = problem.sample_initial_conditions(n_samples=1)
-    assert x0.ndim == 1
-    assert x0.shape[0] == problem.n_states
-    for n_samples in range(2,4):
-        x0 = problem.sample_initial_conditions(n_samples=n_samples)
-        assert x0.ndim == 2
-        assert x0.shape[0] == problem.n_states
-        assert x0.shape[1] == n_samples
 
-#@pytest.mark.parametrize('ocp_name', ocp_dict.keys())
-#def test_cost_functions(ocp_name):
-#    problem = ocp_dict[ocp_name]['ocp']()
+    # Check that sample_initial_conditions returns the correct size arrays
+    for n_samples in range(1,4):
+        x0 = problem.sample_initial_conditions(n_samples=n_samples)
+
+        if n_samples == 1:
+            assert x0.ndim == 1
+            x0 = x0.reshape(-1,1)
+        else:
+            assert x0.ndim == 2
+            assert x0.shape[1] == n_samples
+        assert x0.shape[0] == problem.n_states
+
+@pytest.mark.parametrize('ocp_name', ocp_dict.keys())
+def test_cost_functions(ocp_name):
+    problem = ocp_dict[ocp_name]['ocp']()
+    eps = 1e-06
+
+    for n_samples in range(1,4):
+        # Get some random states and controls
+        x = problem.sample_initial_conditions(n_samples=n_samples)
+        x = x.reshape(problem.n_states, n_samples)
+        u = rng.uniform(low=-1., high=1., size=(problem.n_controls, n_samples))
+
+        # Evaluate the cost functions and check that the shapes are correct
+        L = problem.running_cost(x, u)
+        assert L.ndim == 1
+        assert L.shape[0] == n_samples
+        # Cost functions should also handle flat vector inputs
+        if n_samples == 1:
+            L = problem.running_cost(x.flatten(), u.flatten())
+            assert L.ndim == 0
+
+        try:
+            F = problem.terminal_cost(x)
+            assert np.all(F >= 0.)
+            assert F.ndim == 1
+            assert F.shape[0] == n_samples
+            # Check shapes for flat vector inputs
+            if n_samples == 1:
+                F = problem.terminal_cost(x.flatten())
+                assert F.ndim == 0
+        except NotImplementedError:
+            print('%s OCP has no terminal cost.' % ocp_name)
+
+        # Check that Jacobians give the correct size
+        dLdx, dLdu = problem.running_cost_gradients(x, u)
+        assert dLdx.ndim == dLdu.ndim == 2
+        assert dLdx.shape[1] == dLdu.shape[1] == n_samples
+        assert dLdx.shape[0] == problem.n_states
+        assert dLdu.shape[0] == problem.n_controls
+
+        # Check that Jacobians correspond to finite difference approximations
+
+
+        # Check shapes for flat vector inputs
+        if n_samples == 1:
+            dLdx, dLdu = problem.running_cost_gradients(x.flatten(), u.flatten())
+            assert dLdx.ndim == dLdu.ndim == 1
+            assert dLdx.shape[0] == problem.n_states
+            assert dLdu.shape[0] == problem.n_controls