fixed default and van der pol dynamics jacobians, added test

example_problems/van_der_pol/van_der_pol.py
@@ -190,12 +190,13 @@ class VanDerPol(OptimalControlProblem):
         dx1dt = x2
         dx2dt = self._params.mu * (1. - x1**2) * x2 - x1 + self._params.b * u
 
-        return np.concatenate((dx1dt, dx2dt))
+        return np.concatenate((dx1dt, dx2dt), axis=0)
 
-    def jacobians(self, x, u, f0=None):
+    def jacobians(self, x, u, return_dfdx=True, return_dfdu=True, f0=None):
         '''
         Evaluate the Jacobians of the dynamics with respect to states and
-        controls at single or multiple time instances.
+        controls at single or multiple time instances. Default implementation
+        approximates the Jacobians with central differences.
 
         Parameters
         ----------
@@ -203,30 +204,39 @@ class VanDerPol(OptimalControlProblem):
             State(s) arranged by (dimension, time).
         u : (n_controls,) or (n_controls, n_points) array
             Control(s) arranged by (dimension, time).
+        return_dfdx : bool, default=True
+            If True, compute the Jacobian with respect to states, df/dx.
+        return_dfdu : bool, default=True
+            If True, compute the Jacobian with respect to controls, df/du.
         f0 : ignored
             For API consistency only.
 
         Returns
         -------
-        dfdx : (n_states, n_states, n_points) array
+        dfdx : (n_states, n_states) or (n_states, n_states, n_points) array
             Jacobian with respect to states.
-        dfdu : (n_states, n_controls, n_points) array
+        dfdu : (n_states, n_controls) or (n_states, n_controls, n_points) array
             Jacobian with respect to controls.
         '''
-        print('using finite differences')
-        return super().jacobians(x, u, f0)
+        if return_dfdx:
+            dfdx = np.zeros((self.n_states,) + x.shape)
+            dfdx[0,1] = 1.
+            dfdx[1,0] = -1. - 2.*self._params.mu*x[0]*x[1]
+            dfdx[1,1] = self._params.mu*(1. - x[0]**2)
 
-        x1 = np.atleast_1d(x[0])
-        x2 = np.atleast_1d(x[1])
+            if not return_dfdu:
+                return dfdx
 
-        dfdx = np.array([
-            [np.zeros_like(x1), np.ones_like(x1)],
-            [-1. - 2.*self._params.mu*x1*x2, self._params.mu*(1. - x1**2)]
-        ])
-        dfdu = np.expand_dims(self.B, -1)
-        dfdu = np.tile(dfdu, (1,1,x1.shape[-1]))
+        if return_dfdu:
+            if x.ndim > 1:
+                dfdu = np.tile(self.B[...,None], (1,1,x.shape[-1]))
+            else:
+                dfdu = np.copy(self.B)
 
-        return dfdu, dfdu
+            if not return_dfdx:
+                return dfdu
+
+        return dfdx, dfdu
 
     def optimal_control(self, x, dVdx):
         '''

optimalcontrol/problem.py
@@ -155,7 +155,7 @@ class OptimalControlProblem:
         return_dLdx : bool, default=True
             If True, compute the gradient with respect to states, dL/dx.
         return_dLdu : bool, default=True
-            If True,compute the gradient with respect to controls, dL/du.
+            If True, compute the gradient with respect to controls, dL/du.
 
         Returns
         -------
@@ -202,7 +202,25 @@ class OptimalControlProblem:
         dLdu : (n_controls,) or (n_controls, n_controls, n_points) array
             Control Hessians dL^2/du^2 (x,u) evaluated at pair(s) (x,u).
         '''
-        raise NotImplementedError
+        dLdx, dLdu = self.running_cost_gradients(x, u)
+
+        if return_dLdx:
+            dLdx = approx_derivative(
+                lambda x: self.running_cost_gradients(x, u, return_dLdu=False),
+                x, f0=dLdx
+            )
+            if not return_dLdu:
+                return dLdx
+
+        if return_dLdu:
+            dLdu = approx_derivative(
+                lambda u: self.running_cost_gradients(x, u, return_dLdx=False),
+                u, f0=dLdu
+            )
+            if not return_dLdx:
+                return dLdu
+
+        return dLdx, dLdu
 
     def terminal_cost(self, x):
         '''
@@ -244,7 +262,7 @@ class OptimalControlProblem:
         '''
         raise NotImplementedError
 
-    def jacobians(self, x, u, f0=None):
+    def jacobians(self, x, u, return_dfdx=True, return_dfdu=True, f0=None):
         '''
         Evaluate the Jacobians of the dynamics with respect to states and
         controls at single or multiple time instances. Default implementation
@@ -256,6 +274,10 @@ class OptimalControlProblem:
             State(s) arranged by (dimension, time).
         u : (n_controls,) or (n_controls, n_points) array
             Control(s) arranged by (dimension, time).
+        return_dfdx : bool, default=True
+            If True, compute the Jacobian with respect to states, df/dx.
+        return_dfdu : bool, default=True
+            If True, compute the Jacobian with respect to controls, df/du.
         f0 : (n_states,) or (n_states, n_points) array, optional
             Dynamics evaluated at current state and control pair.
 
@@ -266,42 +288,20 @@ class OptimalControlProblem:
         dfdu : (n_states, n_controls) or (n_states, n_controls, n_points) array
             Jacobian with respect to controls.
         '''
-        x = x.reshape(self.n_states, -1)
-        u = u.reshape(self.n_controls, -1)
-
         if f0 is None:
             f0 = self.dynamics(x, u)
-        f0 = f0.flatten()
 
         # Jacobian with respect to states
-        def f_wrapper(x_flat):
-            x = x_flat.reshape(self.n_states, -1)
-            return self.dynamics(x, u).flatten()
-
-        # Make sparsity pattern
-        sparsity = sparse.hstack([sparse.identity(x.shape[-1])]*self.n_states)
-        sparsity = sparse.vstack([sparsity]*self.n_states)
-
-        dfdx = approx_derivative(
-            f_wrapper, x.flatten(), f0=f0, sparsity=sparsity
-        )
-        dfdx = np.asarray(dfdx[sparsity.nonzero()])
-        dfdx = dfdx.reshape(self.n_states, self.n_states, -1)
+        if return_dfdx:
+            dfdx = approx_derivative(lambda x: self.dynamics(x, u), x, f0=f0)
+            if not return_dfdu:
+                return dfdx
 
         # Jacobian with respect to controls
-        def f_wrapper(u_flat):
-            u = u_flat.reshape(self.n_controls, -1)
-            return self.dynamics(x, u).flatten()
-
-        # Make sparsity pattern
-        sparsity = sparse.hstack([sparse.identity(x.shape[-1])]*self.n_controls)
-        sparsity = sparse.vstack([sparsity]*self.n_states)
-
-        dfdu = approx_derivative(
-            f_wrapper, u.flatten(), f0=f0, sparsity=sparsity
-        )
-        dfdu = np.asarray(dfdu[sparsity.nonzero()])
-        dfdu = dfdu.reshape(self.n_states, self.n_controls, -1)
+        if return_dfdu:
+            dfdu = approx_derivative(lambda u: self.dynamics(x, u), u, f0=f0)
+            if not return_dfdx:
+                return dfdu
 
         return dfdx, dfdu
 

unit_tests/test_OptimalControlProblem.py
@@ -14,7 +14,7 @@ ocp_dict['van_der_pol'] = {
 
 rng = np.random.default_rng()
 
-def _check_finite_differences(x, jac, fun):
+def compare_finite_difference(x, jac, fun):
     expected_jac = approx_derivative(fun, x)
     np.testing.assert_allclose(jac, expected_jac)
 
@@ -87,8 +87,8 @@ def test_cost_functions(ocp_name, n_samples):
     assert dLdx.shape == (problem.n_states, n_samples)
     assert dLdu.shape == (problem.n_controls, n_samples)
 
-    _check_finite_differences(x, dLdx, lambda x: problem.running_cost(x, u))
-    _check_finite_differences(u, dLdu, lambda u: problem.running_cost(x, u))
+    compare_finite_difference(x, dLdx, lambda x: problem.running_cost(x, u))
+    compare_finite_difference(u, dLdu, lambda u: problem.running_cost(x, u))
 
     # Check shapes for flat vector inputs
     if n_samples == 1:
@@ -97,19 +97,53 @@ def test_cost_functions(ocp_name, n_samples):
         assert dLdu.shape == (problem.n_controls,)
 
     # Check that Hessians give the correct size
-    '''dLdx, dLdu = problem.running_cost_hessians(x, u)
-    assert dLdx.ndim == dLdu.ndim == 3
-    assert dLdx.shape[-1] == dLdu.shape[-1] == n_samples
-    assert dLdx.shape[0] == dLdx.shape[1] == problem.n_states
-    assert dLdu.shape[0] == dLdu.shape[1] == problem.n_controls
+    dLdx, dLdu = problem.running_cost_hessians(x, u)
+    assert dLdx.shape == (problem.n_states, problem.n_states, n_samples)
+    assert dLdu.shape == (problem.n_controls, problem.n_controls, n_samples)
 
-    _check_finite_differences(
-        x, u, problem.running_cost_gradients, dLdx, dLdu
+    compare_finite_difference(
+        x, dLdx,
+        lambda x: problem.running_cost_gradients(x, u, return_dLdu=False)
+    )
+    compare_finite_difference(
+        u, dLdu,
+        lambda u: problem.running_cost_gradients(x, u, return_dLdx=False)
     )
 
     # Check shapes for flat vector inputs
-    if False:#n_samples == 1:
-        dLdx, dLdu = problem.running_cost_gradients(x.flatten(), u.flatten())
-        assert dLdx.ndim == dLdu.ndim == 1
-        assert dLdx.shape[0] == problem.n_states
-        assert dLdu.shape[0] == problem.n_controls'''
+    if n_samples == 1:
+        dLdx, dLdu = problem.running_cost_hessians(x.flatten(), u.flatten())
+        assert dLdx.shape == (problem.n_states, problem.n_states)
+        assert dLdu.shape == (problem.n_controls, problem.n_controls)
+
+@pytest.mark.parametrize('ocp_name', ocp_dict.keys())
+@pytest.mark.parametrize('n_samples', range(1,3))
+def test_dynamics(ocp_name, n_samples):
+    problem = ocp_dict[ocp_name]['ocp']()
+
+    # Get some random states and controls
+    x = problem.sample_initial_conditions(n_samples=n_samples)
+    x = x.reshape(problem.n_states, n_samples)
+    u = rng.uniform(low=-1., high=1., size=(problem.n_controls, n_samples))
+
+    # Evaluate the vector and check that the shapes are correct
+    f = problem.dynamics(x, u)
+    assert f.shape == (problem.n_states, n_samples)
+    # Dynamics should also handle flat vector inputs
+    if n_samples == 1:
+        f = problem.dynamics(x.flatten(), u.flatten())
+        assert f.shape == (problem.n_states,)
+
+    # Check that Jacobians give the correct size
+    dfdx, dfdu = problem.jacobians(x, u)
+    assert dfdx.shape == (problem.n_states, problem.n_states, n_samples)
+    assert dfdu.shape == (problem.n_states, problem.n_controls, n_samples)
+
+    compare_finite_difference(x, dfdx, lambda x: problem.dynamics(x, u))
+    compare_finite_difference(u, dfdu, lambda u: problem.dynamics(x, u))
+
+    # Check shapes for flat vector inputs
+    if n_samples == 1:
+        dfdx, dfdu = problem.jacobians(x.flatten(), u.flatten())
+        assert dfdx.shape == (problem.n_states, problem.n_states)
+        assert dfdu.shape == (problem.n_states, problem.n_controls)