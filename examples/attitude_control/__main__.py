fix bug in LQR gradient and hessian calculations when control is saturated

examples/attitude_control/__main__.py
@@ -9,8 +9,8 @@ from sklearn.metrics import r2_score
 
 from optimalcontrol import controls, simulate, open_loop, utilities, analysis
 
-from examples.van_der_pol import VanDerPol
-from examples.van_der_pol import example_config as config
+from examples.attitude_control import AttitudeControl
+from examples.attitude_control import example_config as config
 from examples import example_utilities as example_utils
 
 
@@ -23,11 +23,13 @@ args = parser.parse_args()
 random_seed = getattr(config, 'random_seed', None)
 if random_seed is None:
     random_seed = int(time.time())
-rng = np.random.default_rng(random_seed + 1)
+rng = np.random.default_rng(random_seed + 2)
 
-ocp = VanDerPol(x0_sample_seed=random_seed, **config.params)
-xf = ocp.xf.flatten()
-uf = ocp.uf.flatten()
+ocp = AttitudeControl(attitude_sample_seed=random_seed,
+                      rate_sample_seed=random_seed + 1, **config.params)
+
+xf = np.concatenate((ocp._q_final, np.zeros(3)))
+uf = np.zeros(ocp.n_controls)
 
 # Create an LQR controller as a baseline
 # System matrices (vector field Jacobians)
@@ -38,12 +40,18 @@ lqr = controls.LinearQuadraticRegulator(A=A, B=B, Q=Q, R=R,
                                         u_lb=ocp.parameters.u_lb,
                                         u_ub=ocp.parameters.u_ub,
                                         xf=xf, uf=uf)
+print(A)
+print(B)
+print(Q)
+print(R)
 
 # Generate some training and test data
 
 # First sample initial conditions
 x0_pool = ocp.sample_initial_conditions(config.n_train + config.n_test,
-                                        distance=config.x0_distance)
+                                        )#distance=config.x0_distance)
+
+raise
 
 # Warm start the optimal control solver by integrating the system with LQR
 lqr_sims, status = simulate.monte_carlo_to_converge(

examples/attitude_control/problem_definition.py
@@ -1,16 +1,92 @@
+import warnings
+
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 from optimalcontrol.problem import OptimalControlProblem
-from optimalcontrol.utilities import saturate, resize_vector
+from optimalcontrol.utilities import saturate, find_saturated, resize_vector
 from optimalcontrol.sampling import UniformSampler
 
 
-def cross_product_matrix(w):
-    zeros = np.zeros_like(w[0])
-    wx = np.array([[zeros, -w[2], w[1]],
-                   [w[2], zeros, -w[0]],
-                   [-w[1], w[0], zeros]])
-    return wx
+def cross_product_matrix(omega):
+    """
+
+    Parameters
+    ----------
+    omega
+
+    Returns
+    -------
+
+    """
+    zeros = np.zeros_like(omega[0])
+    return np.array([[zeros, -omega[2], omega[1]],
+                     [omega[2], zeros, -omega[0]],
+                     [-omega[1], omega[0], zeros]])
+
+
+def quaternion_to_euler(quat, degrees=False, normalize=True,
+                        ignore_warnings=True):
+    """
+
+    Parameters
+    ----------
+    quat
+    degrees
+    normalize
+    ignore_warnings
+
+    Returns
+    -------
+    angles
+    """
+    with warnings.catch_warnings():
+        if ignore_warnings:
+            warnings.simplefilter('ignore', category=UserWarning)
+        angles = Rotation(quat.T, normalize=normalize)
+        return angles.as_euler('ZYX', degrees=degrees).T
+
+
+def euler_to_quaternion(yaw, pitch, roll, degrees=False):
+    """
+
+    Parameters
+    ----------
+    yaw
+    pitch
+    roll
+    degrees
+
+    Returns
+    -------
+    quat
+    """
+    angles = np.asarray([yaw, pitch, roll], dtype=float) / 2.
+    if degrees:
+        angles = np.deg2rad(angles)
+
+    cos = np.cos(angles)
+    sin = np.sin(angles)
+
+    yaw_pitch = np.vstack((cos[0] * cos[1],   # cos(yaw/2) * cos(pitch/2)
+                           cos[0] * sin[1],   # cos(yaw/2) * sin(pitch/2)
+                           sin[0] * cos[1],   # sin(yaw/2) * cos(pitch/2)
+                           sin[0] * sin[1]))  # sin(yaw/2) * sin(pitch/2)
+
+    roll = np.vstack((cos[-1], sin[-1]))      # cos(roll/2) * sin(roll/2)
+
+    # Get combinations of yaw, pitch, and roll
+    yaw_pitch_roll = np.einsum('ik,jk->ijk', yaw_pitch, roll)
+
+    q = np.vstack((yaw_pitch_roll[0, 1] - yaw_pitch_roll[3, 0],
+                   yaw_pitch_roll[1, 0] + yaw_pitch_roll[2, 1],
+                   yaw_pitch_roll[2, 0] - yaw_pitch_roll[1, 1],
+                   yaw_pitch_roll[0, 0] + yaw_pitch_roll[3, 1]))
+
+    if angles.ndim == 1:
+        return q[:, 0]
+
+    return q
 
 
 class AttitudeControl(OptimalControlProblem):
@@ -18,9 +94,14 @@ class AttitudeControl(OptimalControlProblem):
                                   [-1.14, 40.56, 0.1],
                                   [-0.8, 0.1, 57.6]],
                             'Wq': 1., 'Ww': 1., 'Wu': 1.,
-                            'x0_max_rate_deg': 5.}
-    _optional_parameters = {'u_lb': -0.3, 'u_ub': 0.3, 'x0_sample_seed': None,
-                            'x0_sample_norm': 2}
+                            'final_attitude': [0., 0., 0.],
+                            'initial_max_attitude': [np.pi, np.pi/2., np.pi],
+                            'initial_max_rate': 0.1,
+                            'attitude_sample_norm': np.inf,
+                            'rate_sample_norm': 2}
+    _optional_parameters = {'u_lb': -0.3, 'u_ub': 0.3,
+                            'attitude_sample_seed': None,
+                            'rate_sample_seed': None}
 
     def _saturate(self, u):
         return saturate(u, self._params.u_lb, self._params.u_ub)
@@ -38,11 +119,24 @@ class AttitudeControl(OptimalControlProblem):
         return np.inf
 
     def _update_params(self, obj, **new_params):
+        if 'final_attitude' in new_params:
+            try:
+                obj.final_attitude = np.abs(obj.final_attitude).reshape(3)
+            except:
+                raise ValueError("final_attitude must be a (3,) array")
+            if np.any(obj.final_attitude > np.pi):
+                raise ValueError("Final pitch and roll must be between -pi and "
+                                 "pi")
+            if obj.final_attitude[1] > np.pi / 2.:
+                raise ValueError("Final yaw must be between -pi/2 and pi/2")
+            self._q_final = euler_to_quaternion(*obj.final_attitude)
+            self._q_final = self._q_final[1:].reshape(3, 1)
+
         if 'J' in new_params:
             try:
                 obj.J = np.reshape(obj.J, (3, 3))
             except:
-                raise ValueError('Inertia matrix J must be (3, 3)')
+                raise ValueError("Inertia matrix J must be (3, 3)")
             self._J = obj.J
             self._JT = self._J.T
             self._Jinv = np.linalg.inv(self._J)
@@ -65,152 +159,186 @@ class AttitudeControl(OptimalControlProblem):
                 u_bound = getattr(obj, key, None)
             setattr(obj, key, u_bound)
 
-        w0_ub = np.deg2rad(np.abs(obj.x0_max_rate_deg))
-        w0_ub = resize_vector(w0_ub, 3)
+        if 'initial_max_attitude' in new_params:
+            try:
+                obj.initial_max_attitude = np.reshape(
+                    np.abs(obj.initial_max_attitude), (3,))
+            except:
+                raise ValueError("initial_max_attitude must be a (3,) array")
+            if np.any(obj.initial_max_attitude > np.pi):
+                raise ValueError("Initial pitch and roll must be between -pi "
+                                 "and pi")
+            if obj.initial_max_attitude[1] > np.pi / 2.:
+                raise ValueError("Initial yaw must be between -pi/2 and pi/2")
 
-        if not hasattr(self, '_x0_sampler'):
+        if not hasattr(self, '_a0_sampler'):
+            self._a0_sampler = UniformSampler(
+                lb=-obj.initial_max_attitude, ub=obj.initial_max_attitude,
+                xf=obj.final_attitude,
+                norm=getattr(obj, 'attitude_sample_norm'),
+                seed=getattr(obj, 'attitude_sample_seed', None))
+        elif ('attitude_sample_seed' in new_params
+              or 'initial_max_attitude' in new_params
+              or 'final_attitude' in new_params):
+            self._a0_sampler.update(
+                lb=-obj.initial_max_attitude, ub=obj.initial_max_attitude,
+                xf=obj.final_attitude,
+                seed=new_params.get('attitude_sample_seed'))
+
+        w0_ub = resize_vector(np.abs(obj.initial_max_rate), 3)
+        if not hasattr(self, '_w0_sampler'):
             self._w0_sampler = UniformSampler(
-                lb=-w0_ub, ub=w0_ub, norm=getattr(obj, 'x0_sample_norm', 2),
-                seed=getattr(obj, 'x0_sample_seed', None))
-        elif 'x0_max_rate_deg' in new_params or 'x0_sample_seed' in new_params:
-            self._x0_sampler.update(lb=-w0_ub, ub=w0_ub,
-                                    seed=new_params.get('x0_sample_seed'))
+                lb=-w0_ub, ub=w0_ub, xf=np.zeros(3),
+                norm=getattr(obj, 'rate_sample_norm'),
+                seed=getattr(obj, 'rate_sample_seed', None))
+        elif ('initial_max_rate' in new_params
+              or 'rate_sample_seed' in new_params):
+            self._w0_sampler.update(
+                lb=-w0_ub, ub=w0_ub, seed=new_params.get('rate_sample_seed'))
 
-    def _break_state(self, X):
-        '''
-        Break up the state vector, [q0, q, w], into individual pieced. If
-        X.shape[0] >= 14, then splits off the remaining states (costates) too.
+    @staticmethod
+    def _break_state(x):
+        """
+        Break up the state vector, `x=[q0, q, w]`, into individual pieces.
 
         Parameters
         ----------
-        X : (7,), (14,), (7, n_samples), or (14, n_samples) array
-            State (and costate, if X.shape[0] > 7)
+        x : (7,) or (7, n_points) array
+            States arranged by dimension, time.
 
         Returns
         -------
-        q0 : (1,) or (1, n_samples) array
-            Scalar component of quaternion
-        q : (4,) or (4, n_samples) array
-            Vector component of quaternion
-        w : (3,) or (3, n_samples) array
-            Angular momenta
-        A0 : (1,) or (1, n_samples) array
-            Costate of scalar component of quaternion
-        Aq : (4,) or (4, n_samples) array
-            Costate of vector component of quaternion
-        Aw : (3,) or (3, n_samples) array
-            Costate of angular momenta
-        '''
-        q0 = X[:1]
-        q = X[1:4]
-        w = X[4:7]
-
-        if X.shape[0] > 7:
-            A0 = X[7:8]
-            Aq = X[8:11]
-            Aw = X[11:14]
-
-            return q0, q, w, A0, Aq, Aw
-
+        q0 : (1,) or (1, n_points) array
+            Scalar component of quaternion, `q0 = x[:1]`.
+        q : (4,) or (4, n_points) array
+            Vector component of quaternion, `q = x[1:4]`.
+        w : (3,) or (3, n_points) array
+            Angular momenta, `w = x[4:7]`.
+        """
+        q0 = x[:1]
+        q = x[1:4]
+        w = x[4:7]
         return q0, q, w
 
-    def sample_X0(self, Ns, dist=None):
-        # Samples angular velocities
-        w = np.random.rand(3, Ns)
-        w = (self.X0_ub[4:] - self.X0_lb[4:]) * w + self.X0_lb[4:]
-
-        # Samples Euler angles
-        v = (2.*np.pi) * np.random.rand(3, Ns) - np.pi
-        v[1] *= 0.5
-
-        # Converts to quaternions
-        cos_v = np.cos(v / 2.)
-        sin_v = np.sin(v / 2.)
-
-        q = np.vstack((
-            -cos_v[0]*sin_v[1]*sin_v[2] + cos_v[1]*cos_v[2]*sin_v[0],
-            cos_v[0]*cos_v[2]*sin_v[1] + sin_v[0]*cos_v[1]*sin_v[2],
-            cos_v[0]*cos_v[1]*sin_v[2] - sin_v[0]*cos_v[2]*sin_v[1]
-        ))
-
-        if dist is not None:
-            if dist <= 1.:
-                w = np.zeros_like(w)
-            else:
-                w_dist = np.sqrt(dist**2 - 1)
-                dist = 1
-                w *= w_dist / np.linalg.norm(w, axis=0)
-
-            q *= dist / np.linalg.norm(q, axis=0)
-
-        # Sets scalar quaternion positive
-        q0 = np.sqrt(1. - np.sum(q**2, axis=0, keepdims=True))
-        X0 = np.vstack((q0, q, w))
-
-        if Ns == 1:
-            X0 = X0.flatten()
-        return X0
-
-    def running_cost(self, X, U):
-        '''
-        Evaluate the running cost L(X,U) at one or multiple state-control pairs.
+    def sample_initial_conditions(self, n_samples=1, attitude_distance=None,
+                                  rate_distance=None):
+        """
+        Generate initial conditions. Euler angles yaw, pitch, roll are sampled
+        uniformly from hypercubes defined by
+        `self.parameters.initial_max_attitude`, then converted to
+        quaternions. Angular rates are sampled uniformly
+        from a hypercube defined by `self.parameters.initial_max_rate`.
+        Optionally, one or both of these may be sampled a specified distance
+        from equilibrium.
 
         Parameters
         ----------
-        X : (n_states,) or (n_states, n_points) array
-            State(s) arranged by (dimension, time).
-        U : (n_controls,) or (n_controls, n_points) array
-            Control(s) arranged by (dimension, time).
+        n_samples : int, default=1
+            Number of sample points to generate.
+        attitude_distance : positive float, optional
+            Desired distance of euler angles from
+            `self.parameters.final_attitude`, in radians. The type of norm is
+            determined by `self.parameters.attitude_sample_norm`. Note that
+            depending on how `distance` is specified, samples may be outside the
+            hypercube defined by `self.parameters.initial_max_attitude`.
+        rate_distance : positive float, optional
+            Desired distance of angular rates from zero, in radians/s. The type
+            of norm is determined by `self.parameters.rate_sample_norm`. Note
+            that depending on how `distance` is specified, samples may be
+            outside the hypercube defined by `self.parameters.initial_max_rate`.
 
         Returns
         -------
-        L : (1,) or (n_points,) array
-            Running cost(s) L(X,U) evaluated at pair(s) (X,U).
-        '''
-        _, q, w = self._break_state(X[:7])
+        x0 : (7, n_samples) or (7,) array
+            Samples of the system state, where each column is a different
+            sample. If `n_samples==1` then `x0` will be a 1d array.
+        """
+        # Sample Euler angles in radians and convert to quaternions
+        angles = self._a0_sampler(n_samples=n_samples,
+                                  distance=attitude_distance)
+        q = euler_to_quaternion(*angles)
+        # Set scalar quaternion positive
+        q[0] = np.abs(q[0])
 
-        return (
-            self.Wq/2. * np.sum(q**2, axis=0)
-            + self.Ww/2. * np.sum(w**2, axis=0)
-            + self.Wu/2. * np.sum(U**2, axis=0)
-        )
+        # Sample angular rates in radians/s
+        w = self._w0_sampler(n_samples=n_samples, distance=rate_distance)
 
-    def running_cost_gradient(self, X, U, return_dLdX=True, return_dLdU=True):
-        '''
-        Evaluate the gradients of the running cost, dL/dX (X,U) and dL/dU (X,U),
-        at one or multiple state-control pairs.
+        return np.concatenate((q, w), axis=0)
 
-        Parameters
-        ----------
-        X : (n_states,) or (n_states, n_points) array
-            State(s) arranged by (dimension, time).
-        U : (n_controls,) or (n_controls, n_points) array
-            Control(s) arranged by (dimension, time).
-        return_dLdX : bool, default=True
-            Set to True to compute the gradient with respect to states, dL/dX.
-        return_dLdU : bool, default=True
-            Set to True to compute the gradient with respect to controls, dL/dU.
+    def running_cost(self, x, u):
+        _, q, w = self._break_state(x)
 
-        Returns
-        -------
-        dLdX : (n_states,) or (n_states, n_points) array
-            Gradient dL/dX (X,U) evaluated at pair(s) (X,U).
-        dLdU : (n_states,) or (n_states, n_points) array
-            Gradient dL/dU (X,U) evaluated at pair(s) (X,U).
-        '''
-        q0, q, w = self._break_state(X[:7])
+        if np.ndim(x) < 2:
+            q_err = q - self._q_final.flatten()
+        else:
+            q_err = q - self._q_final
+        Lq = (self._params.Wq / 2.) * np.sum(q_err**2, axis=0)
+        Lw = (self._params.Ww / 2.) * np.sum(w**2, axis=0)
+        Lu = (self._params.Wu / 2.) * np.sum(self._saturate(u)**2, axis=0)
 
-        if return_dLdX:
-            dLdX = np.concatenate((np.zeros_like(q0), self.Wq * q, self.Ww * w))
-            if not return_dLdU:
-                return dLdX
+        return Lq + Lw + Lu
 
-        if return_dLdU:
-            dLdU = self.Wu * U
-            if not return_dLdX:
-                return dLdU
+    def running_cost_grad(self, x, u, return_dLdx=True, return_dLdu=True,
+                          L0=None):
+        x, u, squeeze = self._reshape_inputs(x, u)
 
-        return dLdX, dLdU
+        if return_dLdx:
+            q0, q, w = self._break_state(x)
+            q_err = q - self._q_final
+            dLdx = np.concatenate((np.zeros_like(q0),
+                                   self._params.Wq * q_err,
+                                   self._params.Ww * w))
+            if squeeze:
+                dLdx = dLdx[..., 0]
+            if not return_dLdu:
+                return dLdx
+
+        if return_dLdu:
+            dLdu = self._params.Wu * self._saturate(u)
+
+            # Where the control is saturated, the gradient is zero
+            sat_idx = find_saturated(u, self._params.u_lb, self._params.u_ub)
+            dLdu[sat_idx] = 0.
+            if squeeze:
+                dLdu = dLdu[..., 0]
+            if not return_dLdx:
+                return dLdu
+
+        return dLdx, dLdu
+
+    def running_cost_hess(self, x, u, return_dLdx=True, return_dLdu=True,
+                          L0=None):
+        x, u, squeeze = self._reshape_inputs(x, u)
+
+        if return_dLdx:
+            Q = np.diag(np.concatenate(([0.], np.full(3, self._params.Wq / 2.),
+                                        np.full(3, self._params.Ww / 2.))))
+            if not squeeze:
+                Q = Q[..., None]
+            if x.shape[1] > 1:
+                Q = np.tile(Q, (1, 1, x.shape[1]))
+
+            if not return_dLdu:
+                return Q
+
+        if return_dLdu:
+            R = np.diag(np.full(3, self._params.Wu / 2.))[..., None]
+            if u.shape[1] > 1:
+                R = np.tile(R, (1, 1, np.shape(u)[1]))
+
+            # Where the control is saturated, the gradient is zero (constant).
+            # This makes the Hessian zero in all terms that include a saturated
+            # control
+            sat_idx = find_saturated(u, self._params.u_lb, self._params.u_ub)
+            sat_idx = sat_idx[None, ...] + sat_idx[:, None, ...]
+            R[sat_idx] = 0.
+
+            if squeeze:
+                R = R[..., 0]
+            if not return_dLdx:
+                return R
+
+        return Q, R
 
     def dynamics(self, X, U):
         '''

examples/van_der_pol/problem_definition.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from optimalcontrol.problem import OptimalControlProblem
-from optimalcontrol.utilities import saturate, resize_vector
+from optimalcontrol.utilities import saturate, find_saturated, resize_vector
 from optimalcontrol.sampling import UniformSampler
 
 
@@ -99,23 +99,31 @@ class VanDerPol(OptimalControlProblem):
 
     def running_cost_grad(self, x, u, return_dLdx=True, return_dLdu=True,
                           L0=None):
-        if np.ndim(x) < 2:
-            x_err = x - self.xf.flatten()
-        else:
-            x_err = x - self.xf
-
-        u_err = self._saturate(u) - self._uf
-
-        x1 = x_err[:1]
-        x2 = x_err[1:]
+        x, u, squeeze = self._reshape_inputs(x, u)
 
         if return_dLdx:
+            x_err = x - self.xf
+            x1 = x_err[:1]
+            x2 = x_err[1:]
+
             dLdx = np.concatenate((self._params.Wx * x1, self._params.Wy * x2))
+
+            if squeeze:
+                dLdx = dLdx[..., 0]
+
             if not return_dLdu:
                 return dLdx
 
         if return_dLdu:
+            u_err = self._saturate(u) - self._uf
             dLdu = self._params.Wu * u_err
+            # Where the control is saturated, the gradient is zero
+            sat_idx = find_saturated(u, self._params.u_lb, self._params.u_ub)
+            dLdu[sat_idx] = 0.
+
+            if squeeze:
+                dLdu = dLdu[..., 0]
+
             if not return_dLdx:
                 return dLdu
 
@@ -131,9 +139,19 @@ class VanDerPol(OptimalControlProblem):
                 return Q
 
         if return_dLdu:
-            R = np.reshape(self._params.Wu / 2., (1, 1))
-            if np.ndim(u) >= 2:
-                R = np.tile(R[..., None], (1, 1, np.shape(u)[1]))
+            R = np.reshape(self._params.Wu / 2., (1, 1, 1))
+            if np.ndim(u) > 1 and np.shape(u)[1] > 1:
+                R = np.tile(R, (1, 1, np.shape(u)[1]))
+
+            # Where the control is saturated, the gradient is zero (constant).
+            # This makes the Hessian zero in all terms that include a saturated
+            # control
+            sat_idx = find_saturated(u, self._params.u_lb, self._params.u_ub)
+            sat_idx = sat_idx[None, ...] + sat_idx[:, None, ...]
+            R[sat_idx] = 0.
+
+            if np.ndim(u) < 2:
+                R = R[..., 0]
             if not return_dLdx:
                 return R
 

optimalcontrol/problem/linear_quadratic.py
@@ -185,13 +185,13 @@ class LinearQuadraticProblem(OptimalControlProblem):
         return cdist(xa, xb, metric='mahalanobis', VI=self.Q)
 
     def running_cost(self, x, u):
-        x, u, squeeze = self._center_inputs(x, u)
+        x_err, u_err, squeeze = self._center_inputs(x, u)
 
         # Batch multiply (x - xf).T @ Q @ (x - xf)
-        L = np.einsum('ij,ij->j', x, self.Q @ x)
+        L = np.einsum('ij,ij->j', x_err, self.Q @ x_err)
 
         # Batch multiply (u - uf).T @ R @ (u - xf) and sum
-        L += np.einsum('ij,ij->j', u, self.R @ u)
+        L += np.einsum('ij,ij->j', u_err, self.R @ u_err)
 
         if squeeze:
             return L[0]
@@ -200,17 +200,23 @@ class LinearQuadraticProblem(OptimalControlProblem):
 
     def running_cost_grad(self, x, u, return_dLdx=True, return_dLdu=True,
                           L0=None):
-        x, u, squeeze = self._center_inputs(x, u)
+        x_err, u_err, squeeze = self._center_inputs(x, u)
 
         if return_dLdx:
-            dLdx = 2. * np.einsum('ij,jb->ib', self.Q, x)
+            dLdx = 2. * np.einsum('ij,jb->ib', self.Q, x_err)
             if squeeze:
                 dLdx = dLdx[..., 0]
             if not return_dLdu:
                 return dLdx
 
         if return_dLdu:
-            dLdu = 2. * np.einsum('ij,jb->ib', self.R, u)
+            dLdu = 2. * np.einsum('ij,jb->ib', self.R, u_err)
+
+            # Where the control is saturated, the gradient is zero
+            sat_idx = find_saturated(np.reshape(u, dLdu.shape),
+                                     self.u_lb, self.u_ub)
+            dLdu[sat_idx] = 0.
+
             if squeeze:
                 dLdu = dLdu[..., 0]
             if not return_dLdx:
@@ -232,9 +238,12 @@ class LinearQuadraticProblem(OptimalControlProblem):
         if return_dLdu:
             dLdu = np.tile(self.R[..., None], (1, 1, u.shape[1]))
 
-            # Where the control is saturated, the gradient is constant so the
-            # Hessian is zero
-            dLdu[:, find_saturated(u, self.u_lb, self.u_ub)] = 0.
+            # Where the control is saturated, the gradient is zero (constant).
+            # This makes the Hessian zero in all terms that include a saturated
+            # control
+            sat_idx = find_saturated(u, self.u_lb, self.u_ub)
+            sat_idx = sat_idx[None, ...] + sat_idx[:, None, :]
+            dLdu[sat_idx] = 0.
 
             if squeeze:
                 dLdu = dLdu[..., 0]

tests/test_LinearQuadraticProblem.py
@@ -188,7 +188,12 @@ def test_cost_functions(n_states, n_controls, n_samples):
     assert dLdx.shape == (n_states, n_samples)
     assert dLdu.shape == (n_controls, n_samples)
 
-    # Check that Hessians give the correct size
+    # Check that control gradients match with finite difference approximation
+    fin_diff_dLdu = (super(LinearQuadraticProblem, ocp)
+                     .running_cost_grad(x, u, return_dLdx=False))
+    np.testing.assert_allclose(dLdu, fin_diff_dLdu, atol=1e-06)
+
+    # Check that Hessians are the correct size
     dLdx2, dLdu2 = ocp.running_cost_hess(x, u)
     assert dLdx2.shape == (n_states, n_states, n_samples)
     assert dLdu2.shape == (n_controls, n_controls, n_samples)
@@ -196,7 +201,7 @@ def test_cost_functions(n_states, n_controls, n_samples):
     # Check that control Hessians match with finite difference approximation
     fin_diff_dLdu2 = (super(LinearQuadraticProblem, ocp)
                       .running_cost_hess(x, u, return_dLdx=False))
-    np.testing.assert_allclose(dLdu2, fin_diff_dLdu2)
+    np.testing.assert_allclose(dLdu2, fin_diff_dLdu2, atol=1e-06)
 
     # Check that vectorized construction matches brute force
     for i in range(n_samples):
@@ -206,8 +211,6 @@ def test_cost_functions(n_states, n_controls, n_samples):
         np.testing.assert_allclose(L[i], xi @ Q @ xi + ui @ R @ ui)
 
         np.testing.assert_allclose(dLdx[..., i], 2. * Q @ xi)
-        np.testing.assert_allclose(dLdu[..., i], 2. * R @ ui)
-
         np.testing.assert_allclose(dLdx2[..., i], Q)
 
     # Check shapes for flat vector inputs