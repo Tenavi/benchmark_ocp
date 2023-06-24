3c27add342699682f02f3ee9dc2730b7db9b1700
Jiequn Han <frankhan91@gmail.com>
2023-06-24 00:22:28 -0400

    Add a linearized UAV example to LQR

--- examples/lqr/__main__.py
+++ examples/lqr/__main__.py
@@ -27,7 +27,6 @@ if random_seed is None:
     random_seed = int(time.time())
 rng = np.random.default_rng(random_seed + 1)
 
-
 ocp = LinearQuadraticProblem(**config.lqr_param_dict, **config.x0_bounds)
 lqr = controls.LinearQuadraticRegulator(**config.lqr_param_dict)
 
@@ -53,7 +52,7 @@ _, x_train, u_train, _, _ = utilities.stack_dataframes(*train_data)
 _, x_test, u_test, _, _ = utilities.stack_dataframes(*test_data)
 
 print("\nTraining neural network controller...")
-controller = supervised_learning.NeuralNetworkController(
+nn_control = supervised_learning.NeuralNetworkController(
     x_train, u_train,
     u_lb=ocp.parameters.u_lb,
     u_ub=ocp.parameters.u_ub,
@@ -61,10 +60,10 @@ controller = supervised_learning.NeuralNetworkController(
     **config.controller_kwargs
 )
 
-print(f"\nLinear stability analysis for {type(controller).__name__:s}:")
+print(f"\nLinear stability analysis for {type(nn_control).__name__:s}:")
 
-x, f = analysis.find_equilibrium(ocp, controller, config.lqr_param_dict['xf'])
-jac = utilities.closed_loop_jacobian(x, ocp.jac, controller)
+x, f = analysis.find_equilibrium(ocp, nn_control, config.lqr_param_dict['xf'])
+jac = utilities.closed_loop_jacobian(x, ocp.jac, nn_control)
 eigs, max_eig = analysis.linear_stability(jac)
 
 # If an unstable equilibrium was found, try perturbing the equilibrium
@@ -72,53 +71,55 @@ eigs, max_eig = analysis.linear_stability(jac)
 if max_eig > 0.:
     x += rng.normal(scale=1/100, size=x.shape)
     t, x, _ = simulate.integrate_to_converge(
-        ocp, controller, x, config.t_int, config.t_max, **config.sim_kwargs)
+        ocp, nn_control, x, config.t_int, config.t_max, **config.sim_kwargs)
 
     print("Closed-loop integration from the unstable equilibrium led to:")
     print(x[:, -1:])
     print("Retrying root-finding...")
 
-    x, f = analysis.find_equilibrium(ocp, controller, x[:, -1])
-    jac = utilities.closed_loop_jacobian(x, ocp.jac, controller)
+    x, f = analysis.find_equilibrium(ocp, nn_control, x[:, -1])
+    jac = utilities.closed_loop_jacobian(x, ocp.jac, nn_control)
     eigs, max_eig = analysis.linear_stability(jac)
 
 print("\n" + "+" * 80)
 
-train_r2 = r2_score(u_train.T, controller(x_train).T)
-test_r2 = r2_score(u_test.T, controller(x_test).T)
-print(f"\n{type(controller).__name__:s} R2 score: {train_r2:.4f} (train) "
+train_r2 = r2_score(u_train.T, nn_control(x_train).T)
+test_r2 = r2_score(u_test.T, nn_control(x_test).T)
+print(f"\n{type(nn_control).__name__:s} R2 score: {train_r2:.4f} (train) "
           f"{test_r2:.4f} (test)")
 
 print("\n" + "+" * 80 + "\n")
 
 # Evaluate performance of the NN controller in closed-loop simulation
-sims, status = simulate.monte_carlo_to_converge(
-    ocp, controller, x0_pool, config.t_int, config.t_max, **config.sim_kwargs)
+nn_sims, status = simulate.monte_carlo_to_converge(
+    ocp, nn_control, x0_pool, config.t_int, config.t_max, **config.sim_kwargs)
 
-for sim in sims:
+for sim in nn_sims:
     sim['v'] = ocp.total_cost(sim['t'], sim['x'], sim['u'])[::-1]
     sim['L'] = ocp.running_cost(sim['x'], sim['u'])
 for sim in lqr_data:
     sim['v'] = ocp.total_cost(sim['t'], sim['x'], sim['u'])[::-1]
+    sim['L'] = ocp.running_cost(sim['x'], sim['u'])
 
 # Plot the results
 figs = {'training': dict(), 'test': dict()}
 for data_idx, data_name in zip((train_idx, test_idx), ('training', 'test')):
     nn_costs = [ocp.total_cost(sim['t'], sim['x'], sim['u'])[-1]
-                for sim in sims[data_idx]]
+                for sim in nn_sims[data_idx]]
     figs[data_name]['cost_comparison'] = plotting.plot_total_cost(
         [sol['v'][0] for sol in lqr_data[data_idx]],
-        controller_costs={f'{type(controller).__name__:s}': nn_costs},
+        controller_costs={f'{type(nn_control).__name__:s}': nn_costs},
         title=f'Closed-loop cost evaluation ({data_name})')
-    
-    ctrl_name = f'{type(controller).__name__:s}'
-    figs[data_name]['closed_loop_' + ctrl_name] = plotting.plot_closed_loop(
-        sims[data_idx], t_max=config.t_int,
-        subtitle=ctrl_name + ', ' + data_name)
-    
-    figs[data_name]['closed_loop_3d' + ctrl_name] = plotting.plot_closed_loop_3d(
-        sims[data_idx], lqr_data[data_idx], controller_name=ctrl_name,
-        title=f'Closed-loop trajectories and controls ({ctrl_name}, 'f'{data_name})')
+    for controller, sims in zip((lqr, nn_control), (lqr_sims, nn_sims)):
+        ctrl_name = f'{type(controller).__name__:s}'
+        figs[data_name]['closed_loop_' + ctrl_name] = plotting.plot_closed_loop(
+            sims[data_idx], t_max=config.t_int,
+            subtitle=ctrl_name + ', ' + data_name)
+
+        if config.lqr_param_dict['xf'].shape[0] <= 4:
+            figs[data_name]['closed_loop_3d' + ctrl_name] = plotting.plot_closed_loop_3d(
+                sims[data_idx], lqr_data[data_idx], controller_name=ctrl_name,
+                title=f'Closed-loop trajectories and controls ({ctrl_name}, 'f'{data_name})')
 
 # Save data, figures, and trained NN
 data_utils.save_data(train_data, os.path.join(config.data_dir, 'train.csv'))
@@ -131,4 +132,5 @@ for data_name, figs_subset in figs.items():
         plt.figure(fig)
         plt.savefig(os.path.join(_fig_dir, fig_name + '.pdf'))
 
-controller.pickle(os.path.join(config.controller_dir, 'nn_control.pickle'))
+lqr.pickle(os.path.join(config.controller_dir, 'lqr.pickle'))
+nn_control.pickle(os.path.join(config.controller_dir, 'nn_control.pickle'))
---
+++ examples/lqr/lin_uav/config.py
@@ -0,0 +1,61 @@
+""""Config file for linearized longitudinal dynamics of a fixed wing UAV as an LQR problem."""
+
+import os
+import numpy as np
+
+# Directories where data, figures, and feedback controllers will be saved
+data_dir = os.path.join('examples', 'lqr', 'lin_uav', 'data')
+fig_dir = os.path.join('examples', 'lqr', 'lin_uav', 'figures')
+controller_dir = os.path.join('examples', 'lqr', 'lin_uav', 'controllers')
+
+for directory in [data_dir, fig_dir, controller_dir]:
+    os.makedirs(directory, exist_ok=True)
+
+random_seed = 123
+problem_name = "Linearized UAV's Longitudinal Dynamics"
+
+# Problem definition definition
+# see lecture matetrial chap5.pdf in https://github.com/randybeard/uavbook and the corresponding uavbook.pdf
+xf = np.array([19.8961, 2.0357, 0, 0.1020, 0])
+uf = np.array([-0.2624, 0.5210])
+A = np.array([[-0.2529, 0.6962, -1.9870, -9.7491, 0], 
+              [-0.6108, -3.6183, 19.4199, -0.9979, 0],
+              [0.3036, -2.9669, -4.2358, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0.1018, -0.9948, 0, 20, 0]])
+B = np.array([[-0.0025, 5.3843], [-1.6575, 0], [-23.1119, 0], [0, 0], [0, 0]])
+Q = np.diag([1/40, 1, (6/np.pi)**2, 1, 1/1000])
+R = np.diag([1/625, 0.1])
+
+x0_lb = xf - np.array([5, 5, np.pi/6, np.pi/3, 10])
+x0_ub = xf + np.array([5, 5, np.pi/6, np.pi/3, 10])
+lqr_param_dict = {
+    'A': A, 'B': B, 'Q': Q, 'R': R,
+    'u_lb': np.array([-0.44, 0]), 'u_ub': np.array([0.44, 1]), 'xf': xf, 'uf': uf
+}
+x0_bounds = {'x0_lb': x0_lb, 'x0_ub': x0_ub}
+
+# Number of training and test trajectories
+# Note: slightly fewer training trajectories may be produced if the solver fails
+# to find open-loop solutions
+n_train = 25
+n_test = 25
+
+# Distance (by default in l-infinity norm) of initial condition samples
+x0_distance = None
+
+# Integration time horizon guess for infinite horizon problems
+t_int = 20
+
+# Maximum integration time allowed
+t_max = 5. * t_int
+
+# Keyword arguments for closed-loop simulation
+sim_kwargs = {'atol': 1e-08, 'rtol': 1e-04, 'method': 'BDF'}
+
+# Keyword arguments for open-loop data generation
+open_loop_kwargs = {}
+
+# Keyword arguments for the SVR controller
+controller_kwargs = {'hidden_layer_sizes': (32, 32), 'activation': 'tanh',
+                     'solver': 'lbfgs', 'max_iter': 10000, 'tol': 1e-03}
